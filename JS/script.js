f8e0b232d0434fd7e47a022c0c52aa3b158450a9
eva-aguilera <119910190+eva-aguilera@users.noreply.github.com>
2023-06-04 20:48:05 -0400

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,73 @@
+/* 
+let numeroUno = Number(prompt("ingrese el numeroUno"))
+let numeroDos = Number(prompt("ingrese el numeroDos"))
+
+//Ejercicio 1
+if (numeroUno > numeroDos) {
+    console.log("el numeroUno es mayor que numeroDos")
+
+}
+
+
+//Ejercicio 2
+if (numeroUno == numeroDos) {
+    console.log("el numeroUno es igual que numeroDos")
+}
+else if (numeroUno !== numeroDos) {
+    console.log("el numeroDos no es igual que numeroUno")
+}
+
+//Ejercicio 3
+if (numeroUno < numeroDos) {
+    console.log("el numerodos es mayor que numeroUno")
+}
+else if (numeroUno === numeroDos) {
+    console.log("el numeroDos es igual que numeroUno")
+}
+
+//Ejercicio 4
+if (numeroUno < numeroDos) {
+    console.log("el numeroDos es mayor que numeroUno")
+}
+else if (numeroUno === numeroDos) {
+    console.log("el numeroDos es igual que numeroUno")
+}
+ */
+
+//Ejercicio 5
+// Definición de los objetos que representan a las personas
+let persona1 = {
+    nombre: "Juan",
+    edad: 25,
+    altura: 180
+};
+
+let persona2 = {
+    nombre: "María",
+    edad: 30,
+    altura: 170
+};
+
+// Comparación de la altura
+if (persona1.altura > persona2.altura) {
+    console.log(persona1.nombre + " es más alta que " + persona2.nombre);
+} else if (persona1.altura < persona2.altura) {
+    console.log(persona2.nombre + " es más alta que " + persona1.nombre);
+} else {
+    console.log("Ambas personas tienen la misma altura");
+}
+
+// Comparación de la edad
+if (persona1.edad > persona2.edad) {
+    console.log(persona1.nombre + " es mayor que " + persona2.nombre);
+} else if (persona1.edad < persona2.edad) {
+    console.log(persona2.nombre + " es mayor que " + persona1.nombre);
+} else {
+    console.log("Ambas personas tienen la misma edad");
+}
+
+//Ejercicio 6
+let nombre =
+    let edad =
+        let altura =
+        let visión =
